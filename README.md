8f4e7b4aef4e29fb55747ef6d340166e49d113c9
Colin Clark <colinbdclark@gmail.com>
2014-01-18 12:41:51 -0500

    colinbdclark/Flocking/gh-48: Adds example app that uses flocking-osc.

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-An example of how Flocking, node-flocking, flocking-osc, and Infusion can be combined to create a hardware-controlled synthesizer using Open Sound Control (OSC).
+An example of how Flocking, flocking-osc, and Infusion can be combined to create a hardware-controlled synthesizer using Open Sound Control (OSC).
--- index.js
+++ index.js
@@ -1,90 +1,12 @@
+"use strict";
+
 var fluid = require("infusion"),
-    flock = fluid.require("flocking");
+    loader = fluid.getLoader(__dirname),
+    flock = fluid.registerNamespace("flock"),
+    colin;
 
-fluid.require("flocking-osc");
-
-var colin = fluid.registerNamespace("colin");
-fluid.registerNamespace("colin.oscFM");
-
-fluid.defaults("colin.oscFM.knobController", {
-    gradeNames: ["flock.io.osc.serial", "autoInit"],
-    
-    components: {
-        oscSource: {
-            options: {
-                model: {
-                    pots: [0, 0, 0, 0]
-                }
-            }
-        }
-    }
-});
-
-fluid.defaults("colin.oscFM.synth", {
-    gradeNames: ["flock.synth", "autoInit"],
-
-    synthDef: {
-        id: "carrier",
-        ugen: "flock.ugen.sin",
-        freq: {
-            ugen: "flock.ugen.value",
-            value: 400,
-            add: {
-                id: "modulator",
-                ugen: "flock.ugen.sin",
-                freq: 124,
-                mul: 100
-            }
-        },
-        mul: {
-            ugen: "flock.ugen.math",
-            source: 307,
-            div: 1024
-        }
-    }
-});
-
-fluid.defaults("colin.oscFM.app", {
-    gradeNames: ["fluid.eventedComponent", "fluid.modelComponent", "autoInit"],
-    
-    components: {
-        controller: {
-            type: "colin.oscFM.knobController",
-            options: {
-                components: {
-                    oscSource: {
-                        options: {
-                            modelListeners: {
-                                "pots.0": {
-                                    funcName: "{synth}.set",
-                                    args: ["carrier.freq.value", "{change}.value"]
-                                },
-                    
-                                "pots.1": {
-                                    funcName: "{synth}.set",
-                                    args: ["modulator.freq", "{change}.value"]
-                                },
-                    
-                                "pots.2": {
-                                    funcName: "{synth}.set",
-                                    args: ["modulator.mul", "{change}.value"]
-                                },
-                                "pots.3": {
-                                    funcName: "{synth}.set",
-                                    args: ["carrier.mul.source", "{change}.value"]
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        },
-        
-        synth: {
-            type: "colin.oscFM.synth"
-        }
-    }
-});
+loader.require("./lib/app.js");
+colin = fluid.registerNamespace("colin");
 
 flock.init();
 flock.enviro.shared.play();
